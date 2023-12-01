118e5cdb91b9e967062a744d073eb7fd4a580571
Oliver Foggin <oliverfoggin@me.com>
2023-12-01 10:39:22 +0000

    Initial Commit

---
+++ AppModeBug/AppModeBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppModeBugApp.swift
+//  AppModeBug
+//
+//  Created by Oliver Foggin on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppModeBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
